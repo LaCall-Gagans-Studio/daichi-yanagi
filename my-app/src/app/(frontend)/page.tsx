04b3e074e42aeae190670c544c9eed73fbc0dcc7
GAGANS STUDIO <142970433+LaCall-Gagans-Studio@users.noreply.github.com>
2025-11-02 02:19:39 +0900

    second launch

--- my-app/src/app/(frontend)/page.tsx
+++ my-app/src/app/(frontend)/page.tsx
@@ -1,62 +1,63 @@
-import { headers as getHeaders } from 'next/headers.js'
-import Image from 'next/image'
-import { getPayload } from 'payload'
 import React from 'react'
-import { fileURLToPath } from 'url'
-
-import config from '@/payload.config'
 import './styles.css'
 
+// sections
+import HomeLinks from '@/sections/home/home-links'
+import HomeMain from '@/sections/home/home-main'
+
 export default async function HomePage() {
-  const headers = await getHeaders()
-  const payloadConfig = await config
-  const payload = await getPayload({ config: payloadConfig })
-  const { user } = await payload.auth({ headers })
-
-  const fileURL = `vscode://file/${fileURLToPath(import.meta.url)}`
-
   return (
-    <div className="home">
-      <div className="content">
-        <picture>
-          <source srcSet="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-favicon.svg" />
-          <Image
-            alt="Payload Logo"
-            height={65}
-            src="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-favicon.svg"
-            width={65}
-          />
-          <div>
-            <h1 className="bg-amber-300">あああああ</h1>
-          </div>
-        </picture>
-        {!user && <h1>Welcome to your new project.</h1>}
-        {user && <h1>Welcome back, {user.email}</h1>}
-        <div className="links">
-          <a
-            className="admin"
-            href={payloadConfig.routes.admin}
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            Go to admin panel
-          </a>
-          <a
-            className="docs"
-            href="https://payloadcms.com/docs"
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            Documentation
-          </a>
+    <div className="w-screen h-screen font-kosugi">
+      <div className="text-black flex w-full h-full">
+        <div id="home-grids" className="bg-ws-primary grow w-1/4">
+          Grids
         </div>
-      </div>
-      <div className="footer">
-        <p>Update this page by editing</p>
-        <a className="codeLink" href={fileURL}>
-          <code>app/(frontend)/page.tsx</code>
-        </a>
+        <div id="home-main" className="bg-white w-sm">
+          <HomeMain />
+        </div>
+        <div id="home-links" className="bg-ws-primary grow">
+          <HomeLinks />
+        </div>
       </div>
     </div>
   )
 }
+
+//   <picture>
+//     <source srcSet="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-favicon.svg" />
+//     <Image
+//       alt="Payload Logo"
+//       height={65}
+//       src="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-favicon.svg"
+//       width={65}
+//     />
+//     <div>
+//       <h1 className="bg-amber-300">あああああ</h1>
+//     </div>
+//   </picture>
+//   {!user && <h1>Welcome to your new project.</h1>}
+//   {user && <h1>Welcome back, {user.email}</h1>}
+//   <div className="links">
+//     <a
+//       className="admin"
+//       href={payloadConfig.routes.admin}
+//       rel="noopener noreferrer"
+//       target="_blank"
+//     >
+//       Go to admin panel
+//     </a>
+//     <a
+//       className="docs"
+//       href="https://payloadcms.com/docs"
+//       rel="noopener noreferrer"
+//       target="_blank"
+//     >
+//       Documentation
+//     </a>
+//   </div>
+// </div>
+// <div className="footer">
+//   <p>Update this page by editing</p>
+//   <a className="codeLink" href={fileURL}>
+//     <code>app/(frontend)/page.tsx</code>
+//   </a>
---
+++ my-app/src/lib/sns-links.ts
@@ -0,0 +1,44 @@
+export const snsLinks = [
+  {
+    name: 'LINE',
+    color: 'bg-[#3fc74e]',
+    url: 'https://line.me',
+    icon: 'https://upload.wikimedia.org/wikipedia/commons/4/41/LINE_logo.svg',
+  },
+  {
+    name: 'YouTube',
+    color: 'bg-[#f02730]',
+    url: 'https://youtube.com',
+    icon: 'https://upload.wikimedia.org/wikipedia/commons/0/0a/YouTube_social_red_circle_%282024%29.svg',
+  },
+  {
+    name: 'X',
+    color: 'bg-black',
+    url: 'https://x.com',
+    icon: 'https://upload.wikimedia.org/wikipedia/commons/c/cc/X_icon.svg',
+  },
+  {
+    name: 'Instagram',
+    color: 'bg-white',
+    url: 'https://instagram.com',
+    icon: 'https://upload.wikimedia.org/wikipedia/commons/e/e7/Instagram_logo_2016.svg',
+  },
+  {
+    name: 'Threads',
+    color: 'bg-black',
+    url: 'https://threads.net',
+    icon: 'https://upload.wikimedia.org/wikipedia/commons/0/01/Threads_%28app%29.svg',
+  },
+  {
+    name: 'TikTok',
+    color: 'bg-white',
+    url: 'https://tiktok.com',
+    icon: 'https://upload.wikimedia.org/wikipedia/commons/a/a6/Tiktok_icon.svg',
+  },
+  {
+    name: 'Facebook',
+    color: 'bg-[#2c68ff]',
+    url: 'https://facebook.com',
+    icon: 'https://upload.wikimedia.org/wikipedia/commons/b/b9/2023_Facebook_icon.svg',
+  },
+]
